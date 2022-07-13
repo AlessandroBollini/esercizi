44c9baa37536b24615fe6ad6e817e90278e3de1c
USERDOMAIN\bollini <bollini.alessandro1998@gmail.com>
2022-07-13 09:26:01 +0200

    commit 2 from dir 1 (fix 1 merge)

--- Hello.java
+++ Hello.java
@@ -1,9 +1,5 @@
 public class Hello{
   public static void main(String [] args){
-<<<<<<< HEAD
     System.out.println("Hello World from dir 1");
-=======
-    System.out.println("Hello World from dir 2");
->>>>>>> 01875e2d49b80eba356c51b8dfe697d46bd31abc
   }
 }
